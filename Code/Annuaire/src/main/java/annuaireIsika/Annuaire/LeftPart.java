7d4fbdf2bc4619758b375988f1590aa0037462a5
Nxmek <Valentin.laveau@gmail.com>
2023-09-01 15:02:20 +0200

    correction

--- Code/Annuaire/src/main/java/annuaireIsika/Annuaire/LeftPart.java
+++ Code/Annuaire/src/main/java/annuaireIsika/Annuaire/LeftPart.java
@@ -4,18 +4,48 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
-public class LeftPart extends VBox{
-	
+public class LeftPart extends VBox {
+
 	public LeftPart() {
 		super(60);
-		
+
 		Label lb = new Label("Recherche ");
 		TextField txtFieldNom = new TextField("Nom ");
+		TextFlow txtFlowNom = new TextFlow(new Text("Nom"));
+
 		TextField txtFieldPrenom = new TextField("Prenom ");
-		Button btnValider = new Button("Valider ");	
-		this.getChildren().addAll(lb,txtFieldNom,txtFieldPrenom,btnValider);
+		TextFlow txtFlowPrenom = new TextFlow(new Text("Prenom"));
+		Button btnValider = new Button("Valider ");
+		this.getChildren().addAll(lb, txtFieldNom, txtFieldPrenom, btnValider);
 		this.setStyle("-fx-background-color: LIGHTGREEN;");
+
+		txtFieldNom.setOnMouseClicked(event -> {
+			if (txtFieldNom.getText().isEmpty()) {
+				txtFlowNom.setVisible(false);
+			}
+		});
+
+		txtFieldNom.focusedProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue && txtFieldNom.getText().isEmpty()) {
+				txtFlowNom.setVisible(true);
+			}
+		});
+
+		txtFieldPrenom.setOnMouseClicked(event -> {
+			if (txtFieldPrenom.getText().isEmpty()) {
+				txtFlowPrenom.setVisible(false);
+			}
+		});
+
+		txtFieldPrenom.focusedProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue && txtFieldPrenom.getText().isEmpty()) {
+				txtFlowPrenom.setVisible(true);
+			}
+		});
+
 	}
 
 }
