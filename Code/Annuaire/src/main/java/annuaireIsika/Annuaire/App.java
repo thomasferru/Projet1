c333dd86f1c45d8327a029e1be6aa2ac05f98faa
Nxmek <Valentin.laveau@gmail.com>
2023-09-01 12:36:55 +0200

    debut de lecture fichier.don (affichage + break après *)

--- Code/Annuaire/src/main/java/annuaireIsika/Annuaire/App.java
+++ Code/Annuaire/src/main/java/annuaireIsika/Annuaire/App.java
@@ -1,5 +1,9 @@
 package annuaireIsika.Annuaire;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -17,9 +21,39 @@ public class App extends Application {
         MainBorderPane root = new MainBorderPane();
         Scene scene = new Scene(root,500,500);
         stage.setScene(scene);
-        stage.show();
+        ParcoursDon();
+//        stage.show();
     }
 
+    public void ParcoursDon() {
+    	/*
+		 * lit le contenu d'un fichier ligne par ligne, crée un Label pour chaque ligne lue,
+		 * puis ajoute chaque Label à une VBox pour afficher les lignes du fichier dans 
+		 * l'interface utilisateur JavaFX. Il gère également les exceptions liées à la lecture
+		 * du fichier en les imprimant dans la console. 
+		  
+		 */
+		
+		
+        try (BufferedReader reader = new BufferedReader(new FileReader("./resources/STAGIAIRES.DON"))) {
+            String line;
+            
+            while ((line = reader.readLine()) != null) {
+            	System.out.println(line);
+            	
+            	if (line.contains("*")){
+            		 break;
+            	}
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+    
     public static void main(String[] args) {
         launch();
     }
