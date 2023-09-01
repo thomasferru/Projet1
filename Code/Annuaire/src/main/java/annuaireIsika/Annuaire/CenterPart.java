6a14bd55d64a52838739f606485ee6edd94f46d8
Nxmek <Valentin.laveau@gmail.com>
2023-09-01 10:40:48 +0200

    Ajout centerPart

---
+++ Code/Annuaire/src/main/java/annuaireIsika/Annuaire/CenterPart.java
@@ -0,0 +1,16 @@
+package annuaireIsika.Annuaire;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class CenterPart extends VBox {
+	public CenterPart() {
+		super(); 
+		
+		Label test = new Label("test"); 
+		
+		this.getChildren().addAll(test);
+		
+	}
+
+}
