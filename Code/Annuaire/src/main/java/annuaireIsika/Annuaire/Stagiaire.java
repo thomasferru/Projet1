8bea0a832a0aeebd38ad61d3fad2b67b1bd4f761
Nxmek <Valentin.laveau@gmail.com>
2023-09-01 15:25:31 +0200

    test push stagiare

--- Code/Annuaire/src/main/java/annuaireIsika/Annuaire/Stagiaire.java
+++ Code/Annuaire/src/main/java/annuaireIsika/Annuaire/Stagiaire.java
@@ -15,8 +15,7 @@ public class Stagiaire {
 
 	// Contructeur a faire en fonction de valentin
 
-	public Stagiaire(String nom, String prenom, int departement, String promotion, int anneeFormation,
-			String filePath) {
+	public Stagiaire(String nom, String prenom, int departement, String promotion, int anneeFormation) {
 		super();
 		this.nom = nom;
 		this.prenom = prenom;
@@ -27,53 +26,56 @@ public class Stagiaire {
 
 	// getter setter
 
+
 	public String getNom() {
 		return nom;
 	}
-
-
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
 	public String getPrenom() {
-		return Prenom;
+		return prenom;
 	}
 
 	public void setPrenom(String prenom) {
-		Prenom = prenom;
+		this.prenom = prenom;
 	}
 
 	public int getDepartement() {
-		return Departement;
+		return departement;
 	}
 
 	public void setDepartement(int departement) {
-		Departement = departement;
+		this.departement = departement;
 	}
 
 	public String getPromotion() {
-		return Promotion;
+		return promotion;
 	}
 
 	public void setPromotion(String promotion) {
-		Promotion = promotion;
+		this.promotion = promotion;
 	}
 
 	public int getAnneeFormation() {
-		return AnneeFormation;
+		return anneeFormation;
 	}
 
 	public void setAnneeFormation(int anneeFormation) {
-		AnneeFormation = anneeFormation;
+		this.anneeFormation = anneeFormation;
 	}
 
+	
 	@Override
 	public String toString() {
-		return "Stagiaire [nom=" + nom + ", Prenom=" + Prenom + ", Departement=" + Departement + ", Promotion="
-				+ Promotion + ", AnneeFormation=" + AnneeFormation + "]";
+		return "Stagiaire [nom=" + nom + ", prenom=" + prenom + ", departement=" + departement + ", promotion="
+				+ promotion + ", anneeFormation=" + anneeFormation + "]";
 	}
+
+
+
 
 	String filePath = "./resources/STAGIAIRES.DON";
 
@@ -84,13 +86,13 @@ public class Stagiaire {
 			String line;
 			String nom = null;
 			String prenom = null;
-			String departement = null;
+			int departement = 0;
 			String classe = null;
 			int anneeRentree = 0;
 			while ((line = reader.readLine()) != null) {
 				if (line.equals("*")) {
-					if (nom != null && prenom != null && departement != null && classe != null && anneeRentree != 0) {
-						Stagiaire stagiaire = new Stagiaire(nom, prenom, departement, classe, anneeRentree);
+					if (nom != null && prenom != null && departement != 0 && classe != null && anneeRentree != 0) {
+						Stagiaire stagiaire = new Stagiaire(nom, prenom, departement, classe, departement);
 						stagiaires.add(stagiaire);
 					}
 				}
