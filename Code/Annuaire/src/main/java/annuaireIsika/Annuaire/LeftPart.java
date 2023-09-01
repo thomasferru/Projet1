d2b115f511690158d105fb44fc01fcf538582ca5
AlexandraAKA <141423433+AlexandraAKA@users.noreply.github.com>
2023-09-01 16:41:34 +0200

    Saisie du texte sans supprimer le textfield

--- Code/Annuaire/src/main/java/annuaireIsika/Annuaire/LeftPart.java
+++ Code/Annuaire/src/main/java/annuaireIsika/Annuaire/LeftPart.java
@@ -15,41 +15,19 @@ public class LeftPart extends VBox {
 
 		Label lb = new Label("Recherche ");
 		
-		TextField txtFieldNom = new TextField("Nom ");
-		TextFlow txtFlowNom = new TextFlow(new Text("Nom"));
-
-		TextField txtFieldPrenom = new TextField("Prenom ");
-		TextFlow txtFlowPrenom = new TextFlow(new Text("Prenom"));
-
-
+		TextField txtFieldNom = new TextField();
+		txtFieldNom.setPromptText("Nom");
+		
+		TextField txtFieldPrenom = new TextField();
+		txtFieldPrenom.setPromptText("Prenom");
+		
+		
 		Button btnValider = new Button("Valider ");	
 		this.getChildren().addAll(lb,txtFieldNom,txtFieldPrenom,btnValider);
 		this.setStyle("-fx-background-color: LIGHTGREEN;");
 		
-		txtFieldNom.setOnMouseClicked(event -> {
-            if (txtFieldNom.getText().isEmpty()) {
-                txtFlowNom.setVisible(false);
-            }
-        });
-		
-		txtFieldNom.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue && txtFieldNom.getText().isEmpty()) {
-            	txtFlowNom.setVisible(true);
-            }
-        });
-		
-		txtFieldPrenom.setOnMouseClicked(event -> {
-            if (txtFieldPrenom.getText().isEmpty()) {
-                txtFlowPrenom.setVisible(false);
-            }
-        });
-		
-		txtFieldPrenom.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue && txtFieldPrenom.getText().isEmpty()) {
-            	txtFlowPrenom.setVisible(true);
-            }
-        });
-	}}
+	}
+	}
 		
 
 		
--- Code/Annuaire/src/main/java/annuaireIsika/Annuaire/RightPart.java
+++ Code/Annuaire/src/main/java/annuaireIsika/Annuaire/RightPart.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class RightPart extends VBox {
 	
@@ -11,14 +13,27 @@ public class RightPart extends VBox {
 		super(24);
 		
 		Label lb = new Label("Recherche avancée ");
-		TextField txtFieldNom = new TextField("Nom ");
-		TextField txtFieldPrenom = new TextField("Prenom ");
-		TextField txtFieldDep = new TextField("Departement ");
-		TextField txtFieldClasse = new TextField("Classe ");
-		TextField txtFieldEntrerForm = new TextField("Entrée en formation ");
+		TextField txtFieldNom = new TextField();
+		txtFieldNom.setPromptText("Nom");
+		
+		TextField txtFieldPrenom = new TextField();
+		txtFieldPrenom.setPromptText("Prenom");
+
+		TextField txtFieldDep = new TextField();
+		txtFieldDep.setPromptText("Departement");
+		
+		TextField txtFieldClasse = new TextField();
+		txtFieldClasse.setPromptText("Classe");
+
+		
+		TextField txtFieldEntreeForm = new TextField();
+		txtFieldEntreeForm.setPromptText("Entrée en formation ");
+
 		Button btnValider = new Button("Valider ");	
-		this.getChildren().addAll(lb,txtFieldNom,txtFieldPrenom,txtFieldDep,txtFieldClasse,txtFieldEntrerForm,btnValider);
+		this.getChildren().addAll(lb,txtFieldNom,txtFieldPrenom,txtFieldDep,txtFieldClasse,txtFieldEntreeForm,btnValider);
 		this.setStyle("-fx-background-color: LIGHTBLUE;");
+		
+		
 	}
 
 }
